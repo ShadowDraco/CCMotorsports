1626e1125e0eee6a08b76171fe94e46c366eef3f
Ethan Storm <stormethan10@gmail.com>
2023-12-22 16:45:28 -0800

    upadate imports

--- cc-motor-sports/components/pageInfo/Reviews.jsx
+++ cc-motor-sports/components/pageInfo/Reviews.jsx
@@ -1,6 +1,6 @@
 'use client'
 import React from 'react'
-import Image from 'next/Image'
+import Image from 'next/image'
 import Link from 'next/link'
 import { Center, Container, Box, Heading } from '@chakra-ui/react'
 import 'react-responsive-carousel/lib/styles/carousel.min.css' // requires a loader
