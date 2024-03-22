9f9eef8f416d2d9e8b99d34735c0832efdb41c19
Ethan Storm <stormethan10@gmail.com>
2024-03-21 18:46:11 -0700

    change logo

--- cc-motor-sports/app/layout.js
+++ cc-motor-sports/app/layout.js
@@ -2,6 +2,7 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import { Providers } from '../components/wrappers/Providers'
 import { Analytics } from '@vercel/analytics/react'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
