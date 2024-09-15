9670731935980c6223828360383a32a0fc3c5201
Ethan Storm <github.balsamic226@passinbox.com>
2024-09-15 14:40:23 -0700

    update map to use client

--- cc-motor-sports/components/CustomGoogleMap.jsx
+++ cc-motor-sports/components/CustomGoogleMap.jsx
@@ -1,17 +1,18 @@
-import { Flex, Center, Button, Box } from '@chakra-ui/react';
-import { useLoadScript, GoogleMap, MarkerF } from '@react-google-maps/api';
+'use client'
+import { Flex, Center, Button, Box } from '@chakra-ui/react'
+import { useLoadScript, GoogleMap, MarkerF } from '@react-google-maps/api'
 
-import { useMemo, useState, useRef } from 'react';
+import { useMemo, useState, useRef } from 'react'
 
 const CustomGoogleMap = () => {
-  const windowSize = useRef([window?.innerWidth, window?.innerHeight]);
-  const mapWidth = windowSize.current[0] - 70;
-  const libraries = useMemo(() => ['places'], []);
+  const windowSize = useRef([window?.innerWidth, window?.innerHeight])
+  const mapWidth = windowSize.current[0] - 70
+  const libraries = useMemo(() => ['places'], [])
   const mapCenter = useMemo(
     () => ({ lat: 34.087791442871094, lng: -117.37747955322266 }),
     []
-  );
-  const [zoom, setZoom] = useState(14);
+  )
+  const [zoom, setZoom] = useState(14)
 
   const mapOptions = useMemo(
     () => ({
@@ -20,47 +21,36 @@ const CustomGoogleMap = () => {
       scrollwheel: false,
     }),
     []
-  );
+  )
 
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
     libraries: libraries,
-  });
+  })
 
   if (!isLoaded) {
-    return <p>Loading...</p>;
+    return <p>Loading...</p>
   }
 
   return (
-    <Box
-      backgroundColor='blackAlpha.300'
-      py={3}
-    >
-      <Flex
-        mt={5}
-        gap={6}
-        w='100%'
-        justifyContent={'center'}
-      >
+    <Box backgroundColor='blackAlpha.300' py={3}>
+      <Flex mt={5} gap={6} w='100%' justifyContent={'center'}>
         <Button
           onClick={() => {
-            setZoom(zoom + 1);
+            setZoom(zoom + 1)
           }}
         >
           +
         </Button>
         <Button
           onClick={() => {
-            setZoom(zoom - 1);
+            setZoom(zoom - 1)
           }}
         >
           -
         </Button>
       </Flex>
-      <Center
-        py={5}
-        id='map'
-      >
+      <Center py={5} id='map'>
         <GoogleMap
           options={mapOptions}
           zoom={zoom}
@@ -76,7 +66,7 @@ const CustomGoogleMap = () => {
         </GoogleMap>
       </Center>
     </Box>
-  );
-};
+  )
+}
 
-export default CustomGoogleMap;
+export default CustomGoogleMap
